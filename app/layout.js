00c0f6aceebf3af4bdd731505a60a5352378538a
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-11-09 15:14:18 +0100

    font

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import { Poppins } from "next/font/google";
 export const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "600", "700"],
+  display: "swap",
 });
 
 export default function RootLayout({ children }) {
