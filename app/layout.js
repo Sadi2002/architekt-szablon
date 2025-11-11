215dd6b98665ff514badb564c2c2966a9b2f4839
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2025-11-11 14:22:42 +0100

    test

--- app/layout.js
+++ app/layout.js
@@ -5,13 +5,22 @@ import { Poppins } from "next/font/google";
 export const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500"],
-  display: "optional",
-  preload: true,
+  display: "swap",
+  preload: false,
 });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pl">
+      <head>
+        {/* Preconnect do Google Fonts, przyspiesza pobranie fontów */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={`${poppins.className} min-h-screen bg-bg-main`}>
         <Header />
         <main>{children}</main>
